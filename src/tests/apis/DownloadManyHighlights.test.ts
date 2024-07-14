86d0e73e669f252456b7bba161dc2e5c5080f061
KanonKC <kanonkce@gmail.com>
2024-07-14 10:52:53 +0700

    Updated unit test

--- src/tests/apis/DownloadManyHighlights.test.ts
+++ src/tests/apis/DownloadManyHighlights.test.ts
@@ -39,19 +39,12 @@ import { downloadManyHighlightsAPI } from "../../apis/DownloadManyHighlight.api"
 // console.log(__dirname)
 // youtube_L1X5z3agHRE_range_3_35_26-3_36_39_QDfi
 downloadManyHighlightsAPI({
-    url: "https://www.youtube.com/live/dsGz1s_7QX0",
+    url: "https://www.twitch.tv/videos/2182932117",
     highlights: [
-        {start: "1:28:40", end: "1:28:46"},
-        {start: "1:39:09", end: "1:39:24"},
-        {start: "2:09:43", end: "2:09:50"},
-        {start: "2:10:08", end: "2:10:24"},
-        {start: "2:12:25", end: "2:12:38"},
-        {start: "2:17:24", end: "2:17:36"},
-        {start: "2:26:43", end: "2:27:07"},
-        {start: "2:43:17", end: "2:43:44"},
+        {start: "4:35:45", end: "4:54:40"},
         // {start: "1:05:32", end: "1:05:47"},
     ],
-    concat: true
+    concat: false
 }).then(response => {
     console.log("Download Completed")
     const FULL_PATH = "C:/Users/user/Documents/Stream-Manage/Streaming-Content-Manager"
