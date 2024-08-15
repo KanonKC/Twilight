64cf57f18f3ec6d29a4611b08a2153c0576418fc
KanonKC <kanonkce@gmail.com>
2024-08-15 21:07:27 +0700

    Feature: Upload One Twitch Highlight To Youtube API

--- src/apis/UploadOneTwitchHighlightToYoutube.api.ts
+++ src/apis/UploadOneTwitchHighlightToYoutube.api.ts
@@ -1,25 +1,26 @@
-// import { twitchDownloadRange } from "../services/twitch-download-range";
-// import { YoutubeUploadVideoDetail, youtubeUpload } from "../services/youtube-upload";
+import { YoutubeUploadVideoDetail } from "../types/Youtube";
+import { twitchDownloadRange } from "../services/downloads/platforms/twitch-download-range";
+import { youtubeUpload } from "../services/uploads/youtube-upload";
 
-// export interface UploadOneTwitchHighlightToYoutubeRequest {
-//     url: string;
-//     highlight: {
-//         start: string;
-//         end: string;
-//     }
-//     videoDetail: YoutubeUploadVideoDetail;
-// }
+export interface UploadOneTwitchHighlightToYoutubeRequest {
+    url: string;
+    highlight: {
+        start: string;
+        end: string;
+    }
+    videoDetail: YoutubeUploadVideoDetail;
+}
 
-// export interface UploadOneTwitchHighlightToYoutubeResponse {
-//     url: string;
-// }
+export interface UploadOneTwitchHighlightToYoutubeResponse {
+    url: string;
+}
 
-// export async function uploadOneTwitchHighlightToYoutubeAPI(payload:UploadOneTwitchHighlightToYoutubeRequest): Promise<UploadOneTwitchHighlightToYoutubeResponse> {
-//     const twitchVideo = await twitchDownloadRange(payload.url,payload.highlight.start,payload.highlight.end)
+export async function uploadOneTwitchHighlightToYoutubeAPI(payload:UploadOneTwitchHighlightToYoutubeRequest): Promise<UploadOneTwitchHighlightToYoutubeResponse> {
+    const twitchVideo = await twitchDownloadRange(payload.url,payload.highlight.start,payload.highlight.end)
     
-//     const targetFilename = twitchVideo.filename
-//     const targetFilePath = `${process.env.VIDEO_STORAGE_PATH}/${targetFilename}`
+    const targetFilename = twitchVideo.filename
+    const targetFilePath = `${process.env.VIDEO_STORAGE_PATH}/${targetFilename}`
     
-//     const uploadResponse = await youtubeUpload(targetFilePath,payload.videoDetail)
-//     return { url: `https://youtube.com/watch?v=${uploadResponse.videoId}`}
-// }
+    const uploadResponse = await youtubeUpload(targetFilePath,payload.videoDetail)
+    return { url: `https://youtube.com/watch?v=${uploadResponse.videoId}`}
+}
