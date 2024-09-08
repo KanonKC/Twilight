ae7225a61f53df6a95ff0191771d30a7d9db7af8
KanonKC <kanonkce@gmail.com>
2024-09-08 13:45:33 +0700

    Remove UploadVideoToYoutube API/controller

--- src/controllers/UploadVideoToYoutube.controller.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import { UploadVideoToYoutubeRequest, uploadVideoToYoutubeAPI } from "../apis/UploadVideoToYoutube.api";
-
-export async function uploadVideoToYoutubeController(request: FastifyRequest, reply: FastifyReply) {
-    return uploadVideoToYoutubeAPI(request.body as UploadVideoToYoutubeRequest)
-}
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,10 @@
 import fastify from "fastify"
 import { downloadVideoController } from "./controllers/DownloadVideo.controller"
-import { uploadVideoToYoutubeController } from "./controllers/UploadVideoToYoutube.controller"
 
 const server = fastify()
 
-// server.post('/highlights', downloadManyHighlights)
-server.post('/youtube', uploadVideoToYoutubeController)
 server.post('/download', downloadVideoController)
+// server.post('/highlights', downloadManyHighlights)
+// server.post('/youtube', uploadVideoToYoutubeController)
 
 export default server
